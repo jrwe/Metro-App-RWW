1f6d47b74cb1dad7100eaf8194def61cd4faf445
John Weldon <jrw@rewireweb.com>
2011-05-20 23:05:18 -0700

    added google map view, wrapped all code in singleton, code cleanup, added module json getter, map view annotations

--- Resources/app.js
+++ Resources/app.js
@@ -1,144 +1,190 @@
-// this sets the background color of the master UIView (when there are no windows/tab groups on it)
-Titanium.UI.setBackgroundColor('#000');
-
-// create tab group
-var tabGroup = Titanium.UI.createTabGroup();
-
-
-//
-// create base UI tab and root window
-//
-var win1 = Titanium.UI.createWindow({  
-    title:'Tab 1',
-    backgroundColor:'#fff'
-});
-var tab1 = Titanium.UI.createTab({  
-    icon:'KS_nav_views.png',
-    title:'Tab 1',
-    window:win1
-});
-
-var label1 = Titanium.UI.createLabel({
-	color:'#999',
-	text:'I am Window 1',
-	font:{fontSize:20,fontFamily:'Helvetica Neue'},
-	textAlign:'center',
-	width:'auto'
-});
-
-win1.add(label1);
-
-//
-// create controls tab and root window
-//
-var win2 = Titanium.UI.createWindow({  
-    title:'Tab 2',
-    backgroundColor:'#fff'
-});
-var tab2 = Titanium.UI.createTab({  
-    icon:'KS_nav_ui.png',
-    title:'Tab 2',
-    window:win2
-});
-
-var label2 = Titanium.UI.createLabel({
-	color:'#999',
-	text:'I am Window 2',
-	font:{fontSize:20,fontFamily:'Helvetica Neue'},
-	textAlign:'center',
-	width:'auto'
-});
-
-
-var xhr = Titanium.Network.createHTTPClient();
-xhr.onerror = function(e)
+(function()
 {
-//	l2.text = e.error;
-};
-
-xhr.onload = onDataLoad;
-
-xhr.open('GET','http://api.metro.net/agencies/lametro/routes/');
-xhr.send();
-
-
-//
-//  add tabs
-//
-tabGroup.addTab(tab1);  
-tabGroup.addTab(tab2);  
-
-
-// open tab group
-tabGroup.open();
-
-function onDataLoad()
-{
-	json = JSON.parse(this.responseText);
-	tableviewAdder(json);
-};
-
-function tableviewAdder(json)
-{
-	// create table view data object
-	var data = [];
-	
-	for(var i = 0; i < json.items.length; i++)
+	var config = 
 	{
-		data.push( {title: json.items[i].display_name,
-			id: json.items[i].id} );
+		endpoints: {
+			baseurl: 'http://api.metro.net/agencies/lametro/',
+			routes: 'routes/',
+			stops: 'routes/{id}/stops/',
+			eventful: 'http://api.eventful.com/json/events/search?keywords={keywords}&location={location}&app_key=4jSM4wsC3xQDkwZV&within=1'
+		}
+	};
+	
+	var xhr = Titanium.Network.createHTTPClient();
+	var win2;
+	var curRoute;
+	var curAnnotation;
+	var curEvent;
+	
+	var init = function()
+	{
+		// this sets the background color of the master UIView (when there are no windows/tab groups on it)
+		Titanium.UI.setBackgroundColor('#000');
+		
+		win2 = Titanium.UI.createWindow({  
+		    title:'Metro Mobile App',
+		    backgroundColor:'#fff'
+		});
+		
+		win2.open({fullscreen:false});
+		
+		xhr.onerror = function(e)
+		{
+			var x = 5;
+			//	l2.text = e.error;
+		};
+		
+		var ep = config.endpoints;
+		jsonGettr(onLoad_routes, ep.baseurl + ep.routes);
 	}
 	
-	// create table view
-	var tableview = Titanium.UI.createTableView({
-		data:data
-	});
+	var jsonGettr = function(callback, url)
+	{
+		xhr.onload = callback;
+		xhr.open('GET', url);
+		xhr.send();
+	};
 	
-	// create table view event listener
-	tableview.addEventListener('click', function(e)
-	{	
-		mapViewr();
-		// event data
-		/*
-		var index = e.index;
-		var section = e.section;
-		var row = e.row;
-		var rowdata = e.rowData;
-		var id = e.rowData.id;
-		var title = e.rowData.title;
-		Ti.API.info('detail ' + e.detail);
-		Titanium.UI.createAlertDialog({title:'Table View',message:'This rows id: ' + id + ' Display name: ' + title + ' section ' + section  + ' row data ' + rowdata}).show();
-		*/
-	});
-	
+	function onLoad_routes()
+	{
+		var json = JSON.parse(this.responseText);
 		
-	// win2.add(label2);
-	win2.add(tableview);
-};
+		// create table view data object
+		var data = [];
+		var routes = json.items;
+		
+		for(var i = 0; i < routes.length; i++)
+		{
+			data.push({
+				title: routes[i].display_name,
+				id: routes[i].id 
+			});
+		}
+		
+		tableviewAdder(data, function(tableview)
+		{
+			// create table view event listener
+			tableview.addEventListener('click', function(e)
+			{
+				curRoute = e.rowData;
+				var ep = config.endpoints;
+				var re = /{id}/gi;
+				
+				var endpoint = ep.baseurl + ep.stops.replace(re, curRoute.id);
+				jsonGettr(onLoad_stops, endpoint);			
+			});
+			
+			// win2.add(label2);
+			win2.add(tableview);
+		});
+	};
+	
+	function tableviewAdder(data, callback)
+	{
+		// create table view
+		var tableview = Titanium.UI.createTableView({
+			data:data
+		});
+		
+		callback(tableview);
+	};
+	
+	function onLoad_stops()
+	{
+		var json = JSON.parse(this.responseText);
+		var stopsArray = [];
+		var stops = json.items;
+		
+		for(var i = 0; i < json.items.length; i++)
+		{
+			var annotation = Titanium.Map.createAnnotation(
+			{
+			    latitude:stops[i].latitude,
+			    longitude:stops[i].longitude,
+			    title:stops[i].display_name,
+			    subtitle:'Click here to see local events in the area!/nPowered by Rewire Web',
+			    pincolor:Titanium.Map.ANNOTATION_RED,
+			    animate:true,
+			    leftButton: 'android/appicon.png',
+			});
+			
+			stopsArray.push(annotation);
+			
+			mapViewr(stopsArray);
+		}
+	};
+			
+	
+	function mapViewr(stopsArray)
+	{
+		var la1 = stopsArray[0].latitude;
+		var lo1 = stopsArray[0].longitude;
+		
+		var la2 = stopsArray[stopsArray.length-1].latitude;
+		var lo2 = stopsArray[stopsArray.length-1].longitude;
+		
+		
+		var mapview = Titanium.Map.createView(
+		{
+		    mapType: Titanium.Map.STANDARD_TYPE,
+		    region: {
+		    	latitude:la1, longitude:lo1, 
+		        latitudeDelta:Math.abs(la1 - la2), longitudeDelta:Math.abs(lo1 - lo2)
+		    },
+		    animate:true,
+		    regionFit:true,
+		    userLocation:true,
+		    annotations:stopsArray
+		});
+		
+		mapview.addEventListener('click', function(e)
+		{						
+			curAnnotation = e.annotation;
+			var curLocation = String(curAnnotation.latitude + ',' + curAnnotation.longitude);
+			var ep = config.endpoints;
+			var re1 = /{keywords}/gi;
+			var re2 = /{location}/gi;
 
-function mapViewr()
-{	
-	var mountainView = Titanium.Map.createAnnotation({
-	    latitude:37.390749,
-	    longitude:-122.081651,
-	    title:"Appcelerator Headquarters",
-	    subtitle:'Mountain View, CA',
-	    pincolor:Titanium.Map.ANNOTATION_RED,
-	    animate:true,
-	    leftButton: '../images/appcelerator_small.png',
-	    myid:1 // CUSTOM ATTRIBUTE THAT IS PASSED INTO EVENT OBJECTS
-	});
+			var endpoint = ep.eventful.replace(re1, 'events');
+			endpoint = endpoint.replace(re2, curLocation);
+			jsonGettr(onLoad_eventful, endpoint);			
+		});
+		
+		var x = 5;
+		// win2.remove(tableview);
+		//win2.add(mapview);
+	};
 	
-	var mapview = Titanium.Map.createView({
-	    mapType: Titanium.Map.STANDARD_TYPE,
-	    region: {latitude:33.74511, longitude:-84.38993, 
-	            latitudeDelta:0.01, longitudeDelta:0.01},
-	    animate:true,
-	    regionFit:true,
-	    userLocation:true,
-	    annotations:[mountainView]
-	});
+	function onLoad_eventful()
+	{
+		var json = JSON.parse(this.responseText);
+		
+		// create table view data object
+		var data = [];
+		var events = json.events.event;
+		
+		for(var i = 0; i < events.length; i++)
+		{
+			data.push({
+				title: events[i].title,
+				latitude: events[i].latitude,
+				longitude: events[i].longitude
+			});
+		}
+		
+		tableviewAdder(data, function(tableview)
+		{
+			// create table view event listener
+			tableview.addEventListener('click', function(e)
+			{
+				curEvent = e.rowData;
+			});
+			
+			// win2.add(label2);
+			win2.add(tableview);
+		});
+	}
 	
-	win2.remove(tableview);
-	win2.add(mapview);
-};
+	
+	init();
+})();
